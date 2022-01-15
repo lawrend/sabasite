6e4652576e759c7757cf58ac8fab7e2fd72604db
lawrend <douglas_lawrence@mac.com>
2022-01-15 15:29:57 -0800

    adjusted file structure

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,14 @@
 <template id="about">
 <main>
+<header>
 I won't dignify your question with an answer
+</header>
+<NuxtLink to="/">HOME</NuxtLink>
 </main>
 
 </template>
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
